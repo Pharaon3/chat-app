a5ba4b5a96287ad3d61736c8c6053fc9580b5499
Pharaon3 <Pharaon313@outlook.com>
2024-01-20 14:06:20 +0900

    update

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const Home = () => {
-  const navigate = useNavigate();
-  const [userName, setUserName] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    localStorage.setItem('userName', userName);
-    navigate('/chat');
-  };
+const Home = ({ socket }) => {
+    const navigate = useNavigate();
+    const [userName, setUserName] = useState('');
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      localStorage.setItem('userName', userName);
+      //sends the username and socket ID to the Node.js server
+      socket.emit('newUser', { userName, socketID: socket.id });
+      navigate('/chat');
+    };
   return (
     <form className="home__container" onSubmit={handleSubmit}>
       <h2 className="home__header">Sign in to Open Chat</h2>
--- server/index.js
+++ server/index.js
@@ -15,16 +15,31 @@ const socketIO = require('socket.io')(http, {
 });
 
 //Add this before the app.get() block
+let users = [];
 socketIO.on('connection', (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
 
   //Listens and logs the message to the console
   socket.on('message', (data) => {
-    console.log(data);
+    socketIO.emit('messageResponse', data);
+  });
+
+  socket.on('newUser', (data) => {
+    //Adds the new user to the list of users
+    users.push(data);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit('newUserResponse', users);
   });
 
   socket.on('disconnect', () => {
     console.log('🔥: A user disconnected');
+    //Updates the list of users when a user disconnects from the server
+    users = users.filter((user) => user.socketID !== socket.id);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit('newUserResponse', users);
+    socket.disconnect();
   });
 });
 
